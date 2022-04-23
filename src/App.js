44e4561555f0b45d762e88aa538f0867282461b8
Nana Kumi <baider11222gmail.com>
2022-04-22 23:51:20 -0400

    submission

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
-import React from 'react';
+import React,{useEffect, useState} from 'react';
 
+import Axios from 'axios'; 
+import './App.css'; 
+import Character from './components/Character'
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
@@ -7,10 +10,24 @@ const App = () => {
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  const [charactersList,setCharactersList] = useState([])
+
+  useEffect(() => {
+    Axios
+      .get("https://swapi.dev/api/people/")
+      .then(res => {
+        setCharactersList(res.data)
+      })
+      .catch(error => {
+        console.log("ERROR")
+      });
+
+  },[])
 
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      <Character charactersData = {charactersList}/>
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,56 @@
 // Write your Character component here
+import React from "react";
+import styled from "styled-components";
+
+
+const StyledContainer = styled.div`
+width: 100vw;
+display: flex;
+justify-content: center;
+border: 5px solid green;
+`;
+
+const StyledDiv = styled.div`
+border: pink solid 5px;
+margin: 10px;
+padding: 3px;
+background-color: black;
+padding: 10px;
+`;
+
+const StyledName = styled.h2`
+color: white;
+font-size: 24px;
+text-transform: uppercase;
+background-color: dodgerblue;
+letter-spacing: 1px;
+`;
+
+const StyledStats = styled.p`
+color: white;
+font-size: 18px;
+line-height: 2;
+`;
+
+
+export default function Character(props) {
+   const { charactersData } = props;
+
+   return (
+    <StyledContainer>
+        {charactersData.map((char) => {
+            return (
+                <StyledDiv>
+                   <StyledName>{char.name}</StyledName>
+                       <StyledStats>
+                           <div>Height: {char.height}</div>
+                           <div>Hair Color: {char.hair_color}</div>
+                           <div>Birth Year:{char.birth_year}</div>
+                           <div>Weight: {char.mass}</div>
+                       </StyledStats>
+               </StyledDiv>
+            )
+        })}; 
+    </StyledContainer>   
+   )
+}
